538aaaf51bf82c24b0f5410b97d17c8f888a37d1
NikolajTrenirovki <nikolya.semenov.1997@list.ru>
2021-06-28 13:29:13 +0300

    1-2

---
+++ src/Returning Strings.kt
@@ -0,0 +1,8 @@
+fun main() {
+    print(greet("Вова"))
+}
+
+fun greet(name: String): String {
+    //your code here
+    return "Hello, $name how are you doing today?"
+}
---
+++ src/String_repeat.kt
@@ -0,0 +1,5 @@
+fun main() {
+    print(repeatStr(0, "строка"))
+}
+
+fun repeatStr(r: Int, str: String) : String = str.repeat(r)
