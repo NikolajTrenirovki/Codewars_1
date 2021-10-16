7a6df775300d5dd519bc171f23fea98f8c2fcc2e
Nikolui333 <semenovnikolu@yandex.ru>
2021-10-16 17:23:28 +0300

    Block_sequence4kyuHelper

--- src/Block_sequence4kyuHelper.kt
+++ src/Block_sequence4kyuHelper.kt
@@ -5,6 +5,7 @@ fun main() {
     var PastResult: Long = 0 // результат сложения всех предидущих разрадов
     var newResult = longArrayOf(0, 0, 0, 0, 0, 0, 0, 0, 0,) // результат сложения текущего разряда (Res1, Res2, Res3...)
     var ResOb: Long = 0 // резьтат сложения всех разрядов (Res общий)
+    var PastResOb: Long = 0 // общей результат до прибавления числе текущего разряда
     var p: Long = 1
     var examination: Long = 0 // проверка (в результате проверки должно получиться 45)
     var examination2: Long = 0 // проверка 2 (в результате проверки должно получиться 45)
@@ -38,7 +39,7 @@ fun main() {
     println("PastResult = $PastResult")
     println("result = $result")
   //  sumResult +=result
-    SumResult += result
+    SumResult = result
     examination2 = result - newResult[1]/2
     println("examination2 = $examination2")
     result = 0
@@ -46,102 +47,138 @@ fun main() {
     examination = ResOb - newResult[1]
     println("examination = $examination")
 
- /*   for(index in 1..999){
+    for(index in 1..999){
         result += index
      }
      println(result)
-     ResOb += (result - SumResult) * p
-     newResult = ResOb - PastResult
+     PastResOb = ResOb
+     ResOb += (result - SumResult) * p // проблема с SumResult
+     newResult[2] = ResOb - PastResOb
      println("p = $p")
-     println("newResult = $newResult")
+     println("newResult = ${newResult[1]}")
      p++
      PastResult += (result - SumResult) * p
-    // sumResult =result
-     examination2 = *//*sumResult -*//* 1493550/3 - 9900/2
+     SumResult =result
+     examination2 = result - newResult[2]/3 - newResult[1]/2
      println("examination2 = $examination2")
      result = 0
      println("ResOb = $ResOb")
-     examination = ResOb - 1493550 - 9900
+     examination = ResOb - newResult[2] - newResult[1]
      println("examination = $examination")
 
     for(index in 1..9999){
         result += index
      }
      println(result)
+     PastResOb = ResOb
      ResOb += (result - SumResult) * p
-     newResult = ResOb - PastResult
+     newResult[3] = ResOb - PastResOb
      println("p = $p")
-     println("newResult = $newResult")
+     println("newResult = ${newResult[3]}")
      p++
      PastResult += (result - SumResult) * p
-     result = 0
+    SumResult =result
+    examination2 = result - newResult[3]/4 - newResult[2]/3 - newResult[1]/2
+    println("examination2 = $examination2")
+    result = 0
+    println("ResOb = $ResOb")
+    examination = ResOb - newResult[3] - newResult[2] - newResult[1]
+    println("examination = $examination")
 
     for(index in 1..99999){
         result += index
      }
      println(result)
+     PastResOb = ResOb
      ResOb += (result - SumResult) * p
-     newResult = ResOb - PastResult
+     newResult[4] = ResOb - PastResOb
      println("p = $p")
-     println("newResult = $newResult")
+     println("newResult = ${newResult[4]}")
      p++
      PastResult += (result - SumResult) * p
-     result = 0
+    SumResult =result
+    examination2 = result - newResult[4]/5 - newResult[3]/4 - newResult[2]/3 - newResult[1]/2
+    println("examination2 = $examination2")
+    result = 0
+    println("ResOb = $ResOb")
+    examination = ResOb - newResult[4] - newResult[3] - newResult[2] - newResult[1]
+    println("examination = $examination")
 
     for(index in 1..999999){
         result += index
      }
      println(result)
+     PastResOb = ResOb
      ResOb += (result - SumResult) * p
-     newResult = ResOb - PastResult
+     newResult[5] = ResOb - PastResOb
      println("p = $p")
-     println("newResult = $newResult")
+     println("newResult = ${newResult[5]}")
      p++
      PastResult += (result - SumResult) * p
-     result = 0
+    SumResult =result
+    examination2 = result - newResult[5]/6 - newResult[4]/5 - newResult[3]/4 - newResult[2]/3 - newResult[1]/2
+    println("examination2 = $examination2")
+    result = 0
+    println("ResOb = $ResOb")
+    examination = ResOb - newResult[5] - newResult[4] - newResult[3] - newResult[2] - newResult[1]
+    println("examination = $examination")
 
     for(index in 1..9999999){
         result += index
      }
      println(result)
+    PastResOb = ResOb
      ResOb += (result - SumResult) * p
-     newResult = ResOb - PastResult
+     newResult[6] = ResOb - PastResOb
      println("p = $p")
-     println("newResult = $newResult")
+     println("newResult = ${newResult[6]}")
      p++
      PastResult += (result - SumResult) * p
-     result = 0
+    SumResult =result
+    examination2 = result - newResult[6]/7 - newResult[5]/6 - newResult[4]/5 - newResult[3]/4 - newResult[2]/3 - newResult[1]/2
+    println("examination2 = $examination2")
+    result = 0
+    println("ResOb = $ResOb")
+    examination = ResOb - newResult[6] - newResult[5] - newResult[4] - newResult[3] - newResult[2] - newResult[1]
+    println("examination = $examination")
 
     for(index in 1..99999999){
         result += index
      }
      println(result)
+     PastResOb = ResOb
      ResOb += (result - SumResult) * p
-     newResult = ResOb - PastResult
+     newResult[7] = ResOb - PastResOb
      println("p = $p")
-     println("newResult = $newResult")
+     println("newResult = ${newResult[7]}")
      p++
      PastResult += (result - SumResult) * p
-     result = 0
+    SumResult =result
+    examination2 = result - newResult[7]/8 - newResult[6]/7 - newResult[5]/6 - newResult[4]/5 - newResult[3]/4 - newResult[2]/3 - newResult[1]/2
+    println("examination2 = $examination2")
+    result = 0
+    println("ResOb = $ResOb")
+    examination = ResOb - newResult[7] - newResult[6] - newResult[5] - newResult[4] - newResult[3] - newResult[2] - newResult[1]
+    println("examination = $examination")
 
      for(index in 1..999999999){
          result += index
      }
      println(result)
+     PastResOb = ResOb
      ResOb += (result - SumResult) * p
-     newResult = ResOb - PastResult
+     newResult[8] = ResOb - PastResOb
      println("p = $p")
-     println("newResult = $newResult")
+     println("newResult = ${newResult[8]}")
      p++
      PastResult += (result - SumResult) * p
-     examination2 = result - 4494949490505050550/9 - 39949494555050550/8 - 349494915050550/7 - 2994946550550/6 - 24949250550/5 - 199475550/4 - 1493550/3 - 9900/2
-     println("examination2 = $examination2")
-     result = 0
-     println("ResOb = $ResOb")
-     examination = ResOb - 4494949490505050550 - 39949494555050550 - 349494915050550 - 2994946550550 - 24949250550 - 199475550 - 1493550 - 9900
-     println("examination = $examination")
-
-        for(index in 1..9999999999){
+    examination2 = result - newResult[8]/9 - newResult[7]/8 - newResult[6]/7 - newResult[5]/6 - newResult[4]/5 - newResult[3]/4 - newResult[2]/3 - newResult[1]/2
+    println("examination2 = $examination2")
+    result = 0
+    println("ResOb = $ResOb")
+    examination = ResOb  - newResult[8] - newResult[7] - newResult[6] - newResult[5] - newResult[4] - newResult[3] - newResult[2] - newResult[1]
+    println("examination = $examination")
+/*        for(index in 1..9999999999){
          result += index
      }
      println(result)
